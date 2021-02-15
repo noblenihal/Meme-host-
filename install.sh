ba37653fddf7dccf3f27d8d38f60a026ec46d87a
nihal gupta <nihalgupta11213@gmail.com>
2021-02-15 14:58:52 +0530

    try to run instal.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,7 @@ sudo apt update
 sudo apt install -y python3-pip
 sudo apt-get install -y python3-venv
 
-cd backend\ -\ xmeme
 
-python3 -m venv venv
-source ./venv/bin/activate
-
-pip3 install -r requirements.txt
+pip3 install -r backend\ -\ xmeme/requirements.txt
 
 
