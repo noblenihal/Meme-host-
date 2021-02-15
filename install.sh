d77ad753898039cdbfb015f6d9a69fc943fd57b0
nihal gupta <nihalgupta11213@gmail.com>
2021-02-15 13:23:40 +0530

    sh files added and updated

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#install.sh
+
+sudo apt update
+sudo apt install -y python3-pip
+sudo apt-get install -y python3-venv
+
+cd nihalgupta11213-me_buildout_xmeme/
+cd backend\ -\ xmeme/
+
+python3 -m venv venv
+source ./venv/bin/activate
+
+pip3 install -r requirements.txt
+
+
